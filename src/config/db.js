d64609d4bd0a4f3a2505e8fd4ab8743e8820f704
MichaelPan24 <1102847670py@gmail.com>
2019-10-29 18:54:21 +0800

    feature: 增加project图片处理中间件

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
 
 
-exports.connect = () => mongoose.connect('mongodb+srv://pan:bnjj1314zyq@ftb-xqfem.mongodb.net/ftb', {
+exports.connect = () => mongoose.connect('mongodb+srv://pan:*******@ftb-xqfem.mongodb.net/ftb', {
     useNewUrlParser: true,
     dbName: 'ftb',
     keepAlive: true
--- src/routers/project.js
+++ src/routers/project.js
@@ -1,7 +1,12 @@
 const express= require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({dest: __dirname+'/../public/upload/caches/'});
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {cb(null, __dirname+'/../public/upload/caches/')},
+    filename: (req, file, cb) => {cb(null, file.fieldname + '-' + Date.now())}
+})
+const upload = multer({storage: storage, limits: {fieldSize: '10MB'}});
 
 const projectController = require('../controllers/projectController');
 
